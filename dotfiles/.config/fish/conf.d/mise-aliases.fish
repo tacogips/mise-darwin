9537d7a3a0ee2c6524dbd47297476e843f2459e0
tacogips <me@tacogips.me>
2026-08-12 17:06:18 +0900

    chore: refine mise and Herdr shortcuts

--- dotfiles/.config/fish/conf.d/mise-aliases.fish
+++ dotfiles/.config/fish/conf.d/mise-aliases.fish
@@ -1,3 +1,4 @@
 alias m='mise'
+alias mt='mise trust'
 alias mupgrade-all='mise -C "$HOME/gits/tacogips/mise-darwin" run upgrade-all'
 alias mupgrade-taco='mise -C "$HOME/gits/tacogips/mise-darwin" run upgrade-taco'
